deacc7f6d40b4b8641be3fa4825424a05c49add5
Scott Isaacson <scott@hitechlowlife.com>
2024-07-31 13:26:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-import logging
-import logging.handlers
+import requests
+from bs4 import BeautifulSoup
 import os
 
-import requests
+# The URL of the page to scrape
+url = "https://developer.ukg.com/wfm/openapi/"
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+# Send a GET request to the URL
+response = requests.get(url)
+response.raise_for_status()  # Raise an error for bad status
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+# Parse the HTML content of the page using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
 
+# Find all <a> tags within <div> tags
+div_tags = soup.find_all('div')
+for div in div_tags:
+    a_tag = div.find('a')
+    if a_tag and 'href' in a_tag.attrs:
+        link = a_tag['href']
+        link_text = a_tag.get_text()
+        filename = f"{link_text}.json"
 
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+        # Construct the full URL for the linked page
+        full_url = "https://developer.ukg.com" + link
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+        # Fetch the content of the linked page
+        linked_page_response = requests.get(full_url)
+        linked_page_response.raise_for_status()
+
+        # Save the content to a file
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(linked_page_response.text)
+
+        print(f"Saved {filename}")
+
+print("All files have been downloaded and saved.")
